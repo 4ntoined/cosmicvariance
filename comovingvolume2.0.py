7ab34b26bb29e83484c47220997344c139a1a11f
Antoine <antoine@LAPTOP-K94B0762.localdomain>
2019-02-19 10:45:43 -0500

    added to comovingvolume2.0 to display ratio between comoving volume and reg volume

--- comovingvolume2.0.py
+++ comovingvolume2.0.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt;
 import numpy as np;
 import math;
 from scipy.integrate import quad;
+from normalvolumemodel import dv
 
 def xE(z):
 	return (omegaM*(1+z)**3+omegaK*(1+z)**2+omegaL)**.5;
@@ -9,7 +10,7 @@ def xE(z):
 def inverseE(z):
 	return 1/xE(z);
 
-def dV(z):
+def dVc(z):
 	entries = np.rint(z*100+1).astype(int)
 	k = entries-1
 	E=(omegaM*(1+z)**3+omegaK*(1+z)**2+omegaL)**.5;
@@ -22,14 +23,16 @@ def dV(z):
 		dM=math.sin((abs(omegaK)**.5)*dC[k])/(abs(omegaK)**.5);
 	if omegaK==0:
 		dM=dC[k];
-	dA=dM/(1+z);
-	dVc=(((1+z)**2)*(dA**2))/E
-	return dVc
+	dA=dM/(1+z);	
+	return (((1+z)**2)*(dA**2))/E
 
 run=0
 x=np.arange(0,5.02,.01)
+y=[dv(b) for b in x] #normal volume element loaded into y 
 while run<3:
-#(1,0,0) (.05,.95,0) (.2,0,.8)
+#(1,0,0) (.05,.95,0) (.2,0,.8) Hogg (2000)
+#matter density Ωm = 0.26, cosmological constant ΩΛ = 0.74, Hubble parameter H0 = 72 km s−1 Mpc−1,
+#fluctuation amplitude σ8 = 0.77 and primordial power-spectrum ns = 0.95. Moster et al. (2009)
 	if run==0:
 		omegaM,omegaK,omegaL=1,0,0
 	if run==1:
@@ -38,16 +41,24 @@ while run<3:
 		omegaM,omegaK,omegaL=.2,0,.8
 
 	if run==0:
-		x1=[dV(a) for a in x]
+		x1=[dVc(a) for a in x]
 	if run==1:
-		x2=[dV(a) for a in x]
+		x2=[dVc(a) for a in x]
 	if run==2:
-		x3=[dV(a) for a in x]
+		x3=[dVc(a) for a in x]
 	run+=1
 
-plt.plot(x,x1)
-plt.plot(x,x2)
-plt.plot(x,x3)
-plt.axis([0,5,0,1.2])
-plt.savefig('comovingvolume.png',dpi=80)
+newy=[y[i] for i in range(len(y))]
+newy[0]=1  #y's first entry is replaced by 1, so as to avoid divide by 0 error
+x4=[x3[i]/newy[i] for i in range(len(x3))]  #dVc/dv is loaded into x4
+
+#plt.plot(x,x1)
+#plt.plot(x,x2)
+#plt.plot(x,x3)
+#plt.axis([0,5,0,1.2])
+plt.plot(x,x4)
+plt.title("ratio of comovingvolume to (non-comoving) volume v. redshift")
+plt.xlabel("redshift")
+plt.ylabel("ratio")
+plt.savefig('volumeratio.png',dpi=80)
 #plt.show();
