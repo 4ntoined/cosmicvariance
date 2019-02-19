04a658bf6212f480afc777dace6ba72d4a38b9a8
Antoine <antoine@LAPTOP-K94B0762.localdomain>
2019-02-19 03:39:05 -0500

    modified code to compute volume by integrating volume element over radius, actually worked out very well

--- normalvolumemodel.py
+++ normalvolumemodel.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
 def volumesphere(r):
 	return (4/3)*math.pi*r**3
@@ -8,8 +9,11 @@ def volumesphere(r):
 def v(side1,side2,r):
 	return (1-math.cos(side1))*side2/(4*math.pi) *volumesphere(r)
 
-def dv(side1,side2,r):
-	return (1-math.cos(side1))*side2*r**2
+def dv(r):
+	return (1-math.cos(10*math.pi/180))*10*math.pi/180*r**2
+
+def cat(r):
+	return quad(dv,0,r)[0]
 
 #s1 and s2 in degrees
 s1=10
@@ -22,12 +26,13 @@ s2=s2*math.pi/180
 
 x=np.arange(0,radius+.01,.01)
 x1=[v(s1,s2,i) for i in x]
-y1=[dv(s1,s2,i) for i in x]
+#y1=[dv(i,s1,s2) for i in x]
+z1=[cat(i) for i in x]
 
-plt.plot(x,y1)
-plt.title("volume element v. redshift")
+plt.plot(x,z1)
+plt.title("volume v. redshift")
 plt.xlabel("radius")
 #plt.ylabel("volume of sphere section")
-plt.ylabel("dV (volume element)")
-plt.savefig('normalvolumeelement.png', dpi=80)
+plt.ylabel("volume")
+plt.savefig('normalvolumeCHECK.png', dpi=80)
 #plt.show()
